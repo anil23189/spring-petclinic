46ec1982c1fd97c3e2a8b29bf2de9a8fc358967b
FWIN01918 <FWIN01918@FLPUNL5RGNYX1.fww.com>
2018-08-31 09:59:37 +0530

    Checking in the changes for making the application connect to oracle

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -21,6 +21,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.SequenceGenerator;
 
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects
@@ -32,7 +33,8 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public class BaseEntity implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQUENCE_PET")
+    @SequenceGenerator(name="SEQUENCE_PET", sequenceName="SEQUENCE_PET", allocationSize=1)
     private Integer id;
 
     public Integer getId() {
